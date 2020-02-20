fd9702cf89c9444fadde9684d4ba52633a357111
Misha K3ler Inozemcev <ikeler1@gmail.com>
2020-02-20 23:32:04 +0200

    Changes

--- src/js/index.js
+++ src/js/index.js
@@ -6,16 +6,3 @@ import GameCtrl from './libs/gameCtrl'
 
 let controller = new GameCtrl(MatrixModel, MapView)
 controller.run()
-
-
-
-/*
-let matrixSize = [3, 3]
-let startPoint = [0, 0]
-let endPoint = [2, 2]
-
-let matrix = new MatrixModel(matrixSize)
-let path = matrix.run(startPoint, endPoint).resultPath;
-
-let map = new MapView("#map")
- */
